5264c28cdcfc09114532c273fa914a0c0d990f34
Filipe Laíns <lains@archlinux.org>
2020-04-27 19:50:06 +0100

    tests: object: adjust test order
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- tests/test_object.py
+++ tests/test_object.py
@@ -5,15 +5,15 @@ import pytest
 from jeepney_objects.object import DBusObjectException, dbus_method
 
 
-def test_call(handlers):
-    assert handlers['ExampleMethod']() == ('s', ('test',))
-
-
 def test_dbus_method(obj):
     assert obj.example_method.is_dbus_method
     assert obj.example_method.dbus_method_name
     assert obj.example_method.dbus_direction
     assert obj.example_method.dbus_signature
+
+
+def test_call(handlers):
+    assert handlers['ExampleMethod']() == ('s', ('test',))
 
 
 def test_dbus_method_error():
