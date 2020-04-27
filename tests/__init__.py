2eb40348359f21917ee5c7586ca65df61d295be9
Filipe Laíns <lains@archlinux.org>
2020-04-27 16:56:14 +0100

    tests: ignore path insert in coverage
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,4 +7,4 @@ f = os.readlink(__file__) if os.path.islink(__file__) else __file__
 path = os.path.realpath(os.path.join(f, '..', '..'))
 
 if path not in sys.path:
-    sys.path.insert(0, path)
+    sys.path.insert(0, path)  # pragma: no cover
