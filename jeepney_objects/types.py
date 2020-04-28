68dc116e0d06b69298b680e7fa1d9a459e56d826
Filipe Laíns <lains@archlinux.org>
2020-04-28 20:48:58 +0100

    types: use TypeVar instead
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- jeepney_objects/types.py
+++ jeepney_objects/types.py
@@ -3,11 +3,11 @@
 import typing
 
 
-Byte = typing.NewType('Byte', int)
-UInt16 = typing.NewType('UInt16', int)
-UInt32 = typing.NewType('UInt32', int)
-UInt64 = typing.NewType('UInt64', int)
-Int16 = typing.NewType('Int16', int)
-Int32 = typing.NewType('Int32', int)
-Int64 = typing.NewType('Int64', int)
-Signature = typing.NewType('Signature', str)
+Byte = typing.TypeVar('Byte', int, int)
+UInt16 = typing.TypeVar('UInt16', int, int)
+UInt32 = typing.TypeVar('UInt32', int, int)
+UInt64 = typing.TypeVar('UInt64', int, int)
+Int16 = typing.TypeVar('Int16', int, int)
+Int32 = typing.TypeVar('Int32', int, int)
+Int64 = typing.TypeVar('Int64', int, int)
+Signature = typing.TypeVar('Signature', str, bytes)
