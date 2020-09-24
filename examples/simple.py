e7d4c69d56d928678709783f2413b63178b15cd6
Filipe Laíns <lains@archlinux.org>
2020-09-24 12:07:58 +0100

    example: add simple example
    Same from README
    
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

---
+++ examples/simple.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import random
+
+from typing import List
+
+import dbus_objects.object
+import dbus_objects.integration.jeepney
+
+
+class ExampleObject(dbus_objects.object.DBusObject):
+    def __init__(self):
+        super().__init__(default_interface_root='io.github.ffy00.dbus_objects.example')
+        self._bets = []
+
+    @dbus_objects.object.dbus_method()
+    def ping(self) -> str:
+        return 'Pong!'
+
+    @dbus_objects.object.dbus_method()
+    def print(self, msg: str) -> None:
+        print(msg)
+
+    @dbus_objects.object.dbus_method()
+    def sum(self, a: int, b: int) -> int:
+        return a + b
+
+    @dbus_objects.object.dbus_method()
+    def save_bet(self, number: int) -> None:
+        self._bets.append(number)
+
+    @dbus_objects.object.dbus_method()
+    def get_bets(self) -> List[int]:
+        return self._bets
+
+    @dbus_objects.object.dbus_method()
+    def lotery(self) -> int:
+        winner = random.choice(self._bets)
+        self._bets = []
+        return winner
+
+
+server = dbus_objects.integration.jeepney.BlockingDBusServer(
+    bus='SESSION',
+    name='io.github.ffy00.dbus-objects'
+)
+server.register_object('/io/github/ffy00/dbus_objects/example', ExampleObject())
+
+server.listen()
