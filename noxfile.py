8dde7ff7ccd41620899e83cd515a8d3570dd708b
Filipe Laíns <lains@riseup.net>
2021-01-16 17:43:15 +0000

    nox: add Python 3.9
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- noxfile.py
+++ noxfile.py
@@ -10,14 +10,14 @@ nox.options.sessions = ['mypy', 'test']
 nox.options.reuse_existing_virtualenvs = True
 
 
-@nox.session(python='3.8')
+@nox.session(python='3.9')
 def mypy(session):
     session.install('.', 'mypy')
 
     session.run('mypy', '-p', 'dbus_objects')
 
 
-@nox.session(python=['3.7', '3.8'])
+@nox.session(python=['3.7', '3.8', '3.9'])
 def test(session):
     htmlcov_output = os.path.join(session.virtualenv.location, 'htmlcov')
     xmlcov_output = os.path.join(session.virtualenv.location, f'coverage-{session.python}.xml')
